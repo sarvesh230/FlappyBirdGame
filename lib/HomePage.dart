524436a13bb293b4b9d400492d4a30a39629a5ee
Sarvesh <sarveshmaggon23@gmail.com>
2021-07-04 18:19:09 +0530

    first commit

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,55 +1,40 @@
 import 'dart:async';
-import 'package:flappybird_app/barriers.dart';
-import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
+
 import 'bird.dart';
+import 'package:flutter/material.dart';
+
 import 'barriers.dart';
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  final bestScoreHive =Hive.box('bestScore');
-
   static double birdYaxis = 0;
-  double height = 0;
   double time = 0;
+  double height = 0;
   double initialHeight = birdYaxis;
-  bool gameHasStarted =false;
-  static double barrierXone = 1.0;
+  bool gameHasStarted = false;
+  double barrierXone = 1.8;
   double barrierXtwo = 1.8 + 1.5;
-  double barrierXthree = 1.8 +3;
-  int score =0;
-  int bestScore = 0;
+  double barrierXthree = 1.8 + 3;
+  bool gameStarted = false;
+  int score = 0;
+  int highscore = 0;
 
   @override
   void initState() {
-      gameHasStarted = false;
-      birdYaxis = 0;
-      time = 0;
-      initialHeight = birdYaxis;
-      barrierXone = 1.8;
-      barrierXtwo = 1.8 + 1.5;
-      barrierXthree = 1.8 +3;
-      score =0;
-      initHiveBestScore();
-      super.initState();
-  }
-  void initHiveBestScore(){
-    bestScore = bestScoreHive.get('score') ?? 0;
-    setState(() {});
-  }
-  void onInit(){
     setState(() {
-      gameHasStarted = false;
       birdYaxis = 0;
       time = 0;
+      height = 0;
       initialHeight = birdYaxis;
       barrierXone = 1.8;
       barrierXtwo = 1.8 + 1.5;
-      barrierXthree = 1.8 +3;
-      score =0;
+      barrierXthree = 1.8 + 3;
+      gameStarted = false;
+      score = 0;
     });
   }
 
@@ -59,106 +44,104 @@ class _HomePageState extends State<HomePage> {
       initialHeight = birdYaxis;
     });
   }
-  bool checkBarrierLost(){
-    if(barrierXone > 0.2 && barrierXone < -0.2){
-      if(birdYaxis < -0.3 || birdYaxis > 0.7){
+
+  bool checkLose() {
+    if (barrierXone < 0.2 && barrierXone > -0.2) {
+      if (birdYaxis < -0.3 || birdYaxis > 0.7) {
         return true;
       }
     }
-    if(barrierXtwo > 0.2 && barrierXtwo < -0.2){
-      if(birdYaxis < -0.8 || birdYaxis > 0.4){
+    if (barrierXtwo < 0.2 && barrierXtwo > -0.2) {
+      if (birdYaxis < -0.8 || birdYaxis > 0.4) {
         return true;
       }
     }
-    if(barrierXthree > 0.2 && barrierXthree < -0.2){
-      if(birdYaxis < -0.4 || birdYaxis > 0.7){
+    if (barrierXthree < 0.2 && barrierXthree > -0.2) {
+      if (birdYaxis < -0.4 || birdYaxis > 0.7) {
         return true;
       }
     }
     return false;
   }
 
-  void startGame(){
+  void startGame() {
     gameHasStarted = true;
-    Timer.periodic(Duration(milliseconds:60), (timer) {
-      time +=0.05;
+    Timer.periodic(Duration(milliseconds: 60), (timer) {
+      time += 0.05;
       height = -4.9 * time * time + 2.8 * time;
       setState(() {
         birdYaxis = initialHeight - height;
-      });
-
-      setState(() {
-        if (barrierXone < -2){
+        if (barrierXone < -2) {
           score++;
           barrierXone += 4.5;
-        }else{
+        } else {
           barrierXone -= 0.04;
         }
-      });
-
-      setState(() {
-        if (barrierXtwo < -2){
+        if (barrierXtwo < -2) {
           score++;
+
           barrierXtwo += 4.5;
-        }else{
+        } else {
           barrierXtwo -= 0.04;
         }
-      });
-      setState(() {
-        if (barrierXthree < -2){
+        if (barrierXthree < -2) {
           score++;
+
           barrierXthree += 4.5;
-        }else{
+        } else {
           barrierXthree -= 0.04;
         }
       });
-      if (birdYaxis > 1 || checkBarrierLost()){
+      if (birdYaxis > 1.3 || checkLose()) {
         timer.cancel();
         _showDialog();
       }
     });
   }
 
-  void _showDialog(){
+  void _showDialog() {
     showDialog(
-      barrierDismissible: false,
         context: context,
-        builder: (BuildContext context){
-      return AlertDialog(
-        backgroundColor: Colors.brown,
-        title: Text("Game Over",style: TextStyle(color: Colors.white),
-        ),
-        content: Text("Score ${score.toString()}",style: TextStyle(color: Colors.white),
-        ),
-        actions: [
-          FlatButton(onPressed: (){
-            if(score > bestScore){
-              bestScore = score;
-               bestScoreHive.put('score', score);
-            }
-            onInit();
-            setState(() {
-              gameHasStarted =false;
-            });
-            Navigator.of(context).pop();
-          },
-            child: Text(
-                "Start Again",
-                style: TextStyle(
-                    color: Colors.white)
-      ),)
-        ],
-      );
-    }
-    );
+        builder: (BuildContext context) {
+          return AlertDialog(
+            backgroundColor: Colors.brown,
+            title: Text(
+              "GAME OVER",
+              style: TextStyle(color: Colors.white),
+            ),
+            content: Text(
+              "Score: " + score.toString(),
+              style: TextStyle(color: Colors.white),
+            ),
+            actions: [
+              FlatButton(
+                child: Text(
+                  "PLAY AGAIN",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {
+                  if (score > highscore) {
+                    highscore = score;
+                  }
+                  initState();
+                  setState(() {
+                    gameHasStarted = false;
+                  });
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
   }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){
+      onTap: () {
         if (gameHasStarted) {
           jump();
-        }else{
+        } else {
           startGame();
         }
       },
@@ -166,70 +149,67 @@ class _HomePageState extends State<HomePage> {
         body: Column(
           children: [
             Expanded(
-              flex: 2,
+                flex: 2,
                 child: Stack(
                   children: [
                     AnimatedContainer(
-                      alignment: Alignment(0,birdYaxis),
+                      alignment: Alignment(0, birdYaxis),
                       duration: Duration(milliseconds: 0),
                       color: Colors.blue,
                       child: MyBird(),
                     ),
                     Container(
-                      alignment: Alignment(0,-0.3),
-                      child: gameHasStarted ? Text(" ") :
-                      Text("T A P   T O   P L A Y",
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.white,
-                        ),
-                      ),
+                      alignment: Alignment(0, -0.3),
+                      child: gameHasStarted
+                          ? Text(" ")
+                          : Text("T A P  T O  P L A Y",
+                              style:
+                                  TextStyle(fontSize: 20, color: Colors.white)),
                     ),
                     AnimatedContainer(
-                      alignment: Alignment(barrierXone,1.1),
+                      alignment: Alignment(barrierXone, 1.1),
                       duration: Duration(milliseconds: 0),
                       child: MyBarrier(
                         size: 200.0,
                       ),
                     ),
                     AnimatedContainer(
-                      alignment: Alignment(barrierXone,1.1),
+                      alignment: Alignment(barrierXtwo, 1.1),
                       duration: Duration(milliseconds: 0),
                       child: MyBarrier(
                         size: 300.0,
                       ),
                     ),
                     AnimatedContainer(
-                      alignment: Alignment(barrierXtwo,-1.1),
+                      alignment: Alignment(barrierXthree, 1.1),
                       duration: Duration(milliseconds: 0),
                       child: MyBarrier(
                         size: 100.0,
                       ),
                     ),
                     AnimatedContainer(
-                      alignment: Alignment(barrierXtwo,-1.1),
+                      alignment: Alignment(barrierXone, -1.1),
                       duration: Duration(milliseconds: 0),
                       child: MyBarrier(
                         size: 150.0,
                       ),
                     ),
                     AnimatedContainer(
-                      alignment: Alignment(barrierXthree,-1.1),
+                      alignment: Alignment(barrierXtwo, -1.1),
                       duration: Duration(milliseconds: 0),
                       child: MyBarrier(
                         size: 100.0,
                       ),
                     ),
                     AnimatedContainer(
-                      alignment: Alignment(barrierXthree,-1.1),
+                      alignment: Alignment(barrierXthree, -1.1),
                       duration: Duration(milliseconds: 0),
                       child: MyBarrier(
                         size: 200.0,
                       ),
                     ),
                   ],
-                ),
-            ),
+                )),
             Container(
               height: 15,
               color: Colors.green,
@@ -237,27 +217,35 @@ class _HomePageState extends State<HomePage> {
             Expanded(
               child: Container(
                 color: Colors.brown,
-                child: Row (
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text("SCORE",style: TextStyle(color: Colors.white ,fontSize: 20),),
+                        Text("SCORE",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 20)),
                         SizedBox(
                           height: 20,
                         ),
-                        Text(score.toString(),style: TextStyle(color: Colors.white ,fontSize: 35),),
+                        Text(score.toString(),
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 35)),
                       ],
                     ),
                     Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text("BEST",style: TextStyle(color: Colors.white ,fontSize: 20),),
+                        Text("BEST",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 20)),
                         SizedBox(
                           height: 20,
                         ),
-                        Text(bestScore.toString(),style: TextStyle(color: Colors.white ,fontSize: 35),),
+                        Text(highscore.toString(),
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 35)),
                       ],
                     ),
                   ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flappybird_app/HomePage.dart';
-import 'package:hive/hive.dart';
-import 'package:path_provider/path_provider.dart' as path_provider;
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  final appDocumentDirectory =
-      await path_provider.getApplicationDocumentsDirectory();
-  Hive.init(appDocumentDirectory.path);
-  await Hive.openBox('bestScore');
+//import 'package:hive/hive.dart';
+//import 'package:path_provider/path_provider.dart' as path_provider;
+
+void main() {
   runApp(MyApp());
 }
 
@@ -16,12 +12,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
       home: HomePage(),
     );
   }
 }
-
+//async {
+  //WidgetsFlutterBinding.ensureInitialized();
+  //final appDocumentDirectory =
+    //  await path_provider.getApplicationDocumentsDirectory();
+  //Hive.init(appDocumentDirectory.path);
+  //await Hive.openBox('bestScore');
